58e7202ae0fc025432b93a600906e867afda0008
Трапизонян Рафик <leorafik29@gmail.com>
2022-05-12 02:39:39 +0300

    index

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.resolve(__dirname, '/client/build')))
+app.use(express.static(path.resolve(__dirname, '../client/build')))
+console.log(path.resolve(__dirname, '../client/build'))
 app.use(fileUpload({}))
 app.use('/api', router)
 
